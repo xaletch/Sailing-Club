2f167f3f9d5f3ada7ecf6981ea3687781374f495
xaletch <125193708+xaletch@users.noreply.github.com>
2023-06-11 17:02:33 +0300

    Update theme.js

--- sail-club/js/theme.js
+++ sail-club/js/theme.js
@@ -1,6 +1,6 @@
 let body = document.querySelector('body');
 let icon = document.querySelector('.theme-item');
-let logo = document.querySelector('.logo', 'footer-logo');
+let logo = document.querySelectorAll('.logo-svg');
 
 document.querySelector('.theme').addEventListener('click', (e) => {
     e.preventDefault();
@@ -13,16 +13,21 @@ document.querySelector('.theme').addEventListener('click', (e) => {
 });
 
 function addDark () {
-    try {
-        if (localStorage.getItem('theme') === 'dark') {
-            body.classList.add('dark');
-            icon.classList.add('bx-sun');
-            logo.classList.add('logo-dark')
-        } else {
-            body.classList.remove('dark');
-            icon.classList.remove('bx-sun');
-            logo.classList.remove('logo-dark')
-        }
-    } catch (err) {}
+    let theme = localStorage.getItem('theme');
+    body.classList.toggle('dark', theme === 'dark');
+    icon.classList.toggle('bx-sun', theme === 'dark');
+    logo.forEach(logo => logo.classList.toggle('dark-svg', theme === 'dark'));
+    
+    // try {
+    //     if (localStorage.getItem('theme') === 'dark') {
+    //         body.classList.add('dark');
+    //         icon.classList.add('bx-sun');
+    //         logo.classList.add('logo-dark')
+    //     } else {
+    //         body.classList.remove('dark');
+    //         icon.classList.remove('bx-sun');
+    //         logo.classList.remove('logo-dark')
+    //     }
+    // } catch (err) {}
 }
 addDark();
